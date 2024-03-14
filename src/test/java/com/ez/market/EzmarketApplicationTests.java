160be2c8ce847953378e0f8c940d7618909cc103
VividVortex <vovodla74@gmail.com>
2024-03-14 17:06:24 +0900

    feat : DTO 추가

---
+++ src/test/java/com/ez/market/EzmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ez.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
