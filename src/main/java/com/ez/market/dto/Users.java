a28fac605e5c41e98c58cc49b29b1a7c9ba538c5
VividVortex <vovodla74@gmail.com>
2024-03-15 12:39:22 +0900

    Revert "feat:VO추가"
    This reverts commit 570d144a63c8053d92accddce71a5329aef74537.

--- src/main/java/com/ez/market/dto/Users.java
+++ src/main/java/com/ez/market/dto/Users.java
@@ -1,42 +1,11 @@
 package com.ez.market.dto;
 
-import org.springframework.lang.NonNull;
-
-
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-@RequiredArgsConstructor
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Data
 public class Users 
 {
-	@Id
-	@NonNull
-	private String userid;
-	@Column(nullable = false)
-	private String userpwd;
-	@Column(nullable = false)
-	private String username;
-	@Column(nullable = false)
-	private String email;
-	@Column(nullable = false)
-	private String phone;
-	@Column(nullable = false)
-	private java.sql.Date regdate;
-	@Column(nullable = false)
-	private String enabled;
-	@Column(nullable = false)
-	private String gender;
-	@Column(nullable = false)
-	private int age;
+	
 }
