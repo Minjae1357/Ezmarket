801a6e89779e768b4072e304654c26b34119b73a
Jomingyu0408 <jomingyu0408@gmail.com>
2024-03-17 20:56:52 +0900

    @Table(name="order_info")

--- src/main/java/com/ez/market/dto/OrderInfo.java
+++ src/main/java/com/ez/market/dto/OrderInfo.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @Entity
 @NoArgsConstructor
 @RequiredArgsConstructor
+@Table(name="order_info")
 public class OrderInfo
 {
 	@Id
