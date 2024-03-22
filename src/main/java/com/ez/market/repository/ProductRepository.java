c9eae289c13430797e13dbe21fb4e9b24bb90ba2
Akuamu <sskang6517@gmail.com>
2024-03-22 16:05:16 +0900

    refactor : Repository 병합

--- src/main/java/com/ez/market/repository/ProductRepository.java
+++ src/main/java/com/ez/market/repository/ProductRepository.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ez.market.dto.Product;
 
-<<<<<<< HEAD
-public interface ProductRepository extends JpaRepository<Product,String>
-=======
-public interface ProductRepository extends JpaRepository<product, Integer>
->>>>>>> ae252ac34f3cb6172deae5a2f7e16fa0012341fa
+
+public interface ProductRepository extends JpaRepository<Product, Integer>
 {
 
 }
