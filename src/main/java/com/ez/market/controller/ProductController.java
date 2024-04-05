e9fcd40ac92377581487f90b78a32af5e2de1e03
VividVortex <vovodla74@gmail.com>
2024-04-05 11:36:32 +0900

    refactor : brands 업로드기능 개선

--- src/main/java/com/ez/market/controller/ProductController.java
+++ src/main/java/com/ez/market/controller/ProductController.java
@@ -76,39 +76,15 @@ public class ProductController {
 	
 	@PostMapping("/addBrand")
 	@ResponseBody
-	public Map<String,Object> upload(HttpServletRequest request,@RequestParam("brand") String brandName,@RequestParam("files")MultipartFile[] files) 
+	public Map<String,Object> upload(@RequestParam("brand") String brandName,@RequestParam("files")MultipartFile[] files) 
 	{
-		System.out.println("brand이름");
 		Map<String,Object> map = new HashMap<>();
-		
-		System.out.println("brand이름"+brandName);
-		//브랜드 객체 생성 및 이름 설정
-		Brands brand = new Brands();
-	    brand.setBrandName(brandName);
-	    // 이미지 파일을 저장할 경로
-	    ServletContext context = request.getServletContext();
-	    String uploadPath = context.getRealPath("/WEB-INF/images/brands");
-	    File uploadDir = new File(uploadPath);
-	     // 이미지 파일을 서버에 저장하고, 저장된 경로를 브랜드 객체에 설정
         try {
-            for (MultipartFile file : files) {
-                String fileName = file.getOriginalFilename();
-                String filePath = uploadPath + File.separator + fileName;
-                File dest = new File(filePath);
-                file.transferTo(dest); // 파일 저장
-
-                // 브랜드 객체에 이미지 파일의 경로 설정
-                brand.setBrandImg(filePath);
-            }
-            boolean addBrand = brandSvc.saveBrand(brandName,uploadPath);
+        	log.info("브랜드이름"+brandName);
+        	log.info("파일"+files);
+            boolean addBrand = brandSvc.saveBrand(files,brandName);
             map.put("added",addBrand);
             return map;
-        } catch (IOException e) {
-            e.printStackTrace();
-            map.put("added", false); // 실패 시 응답 설정
-            map.put("error", "File upload failed: " + e.getMessage()); // 실패 이유 추가
-            return map;
-            // 나중에 트랜잭션으로 연결하는게 좋아보임.
         } catch (Exception e) {
             e.printStackTrace();
             map.put("added", false); // 실패 시 응답 설정
