c5d7a4ed74639dc094e93e314036a7322b433eae
[Github <[Github>
2024-04-23 21:16:00 +0900

    Feat : 메인페이지 구현 및 사이드바와상품리스트연결

--- src/main/java/com/ez/market/controller/MainPageController.java
+++ src/main/java/com/ez/market/controller/MainPageController.java
@@ -1,5 +1,11 @@
 package com.ez.market.controller;
 
+import com.ez.market.dto.Imgs;
+import com.ez.market.dto.Product;
+import com.ez.market.dto.ProductBoard;
+import com.ez.market.service.ImgsService;
+import com.ez.market.service.ProductBoardService;
+import com.ez.market.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ez.market.service.CartService;
 
+import java.util.List;
+
 //메인페이지관련된 요청은여기서
 @Controller
 @RequestMapping("/main")
@@ -17,13 +25,25 @@ public class MainPageController
 {
 	@Autowired
 	CartService cartsvc;
-	
+	@Autowired
+	ImgsService imgSvc;
+	@Autowired
+	ProductService pSvc;
+	@Autowired
+	ProductBoardService PBSvc;
+
 	@GetMapping("menu")
 	public String main(Model m) {
 		//메인페이지 장바구니아이콘에 숫자 디폴트값을 0으로해두고만들어야한다.
 		
 		int count = cartsvc.cartCount();
-		
+		List<ProductBoard> pblist = PBSvc.findTop30ByOrderByPnumDesc();
+		System.out.println("pblist" + pblist);
+		List<Product> plist = pSvc.findProductsByPnums(pblist);
+		List<Imgs> ilist = imgSvc.findImgsByPId(plist);
+		m.addAttribute("productBoard",pblist);
+		m.addAttribute("product",plist);
+		m.addAttribute("img",ilist);
 		m.addAttribute("count" ,count);
 		return "main/menu";
 	}
