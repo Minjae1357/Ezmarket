e3167083954b141bedc03a1c877c46dc3404e3bf
Jomingyu0408 <jomingyu0408@gmail.com>
2024-05-02 12:31:24 +0900

    Revert "장바구니 검색기능 추가, 장바구니 중복담기 방지기능 추가"
    This reverts commit dc2f9f21aeb203713d75679e176bfe608035ab97.

--- src/main/java/com/ez/market/controller/CartController.java
+++ src/main/java/com/ez/market/controller/CartController.java
@@ -80,7 +80,6 @@ public class CartController
 		List<CartPage> cartList = cartsvc.getCartList();
 		System.out.println("엄준식" + cartList);
 		model.addAttribute("cartList", cartList); 
-		model.addAttribute("searchtext", "");
 		return "cart/listPage";
 	}
 	
@@ -169,16 +168,6 @@ public class CartController
 		map.put("receipted", receipted); 
 		return map;
 	}
-	
-	// 장바구니 검색
-	@GetMapping("search")
-	public String search(@RequestParam String searchtext, Model model) {
-		List<CartPage> cartSearchList = cartsvc.getCartSearchList(searchtext);
-		model.addAttribute("cartList", cartSearchList); 
-		model.addAttribute("searchtext", searchtext);
-		return "cart/listPage";
-	}
-	
 	
 }
 
--- src/main/java/com/ez/market/controller/ProductBoardController.java
+++ src/main/java/com/ez/market/controller/ProductBoardController.java
@@ -98,16 +98,6 @@ public class ProductBoardController
 		return"product/productDetail";
 	}
 	
-	@GetMapping("CartOverlapCheck")
-	@ResponseBody
-	public Map<String,Boolean> cartOverlapCheck(@RequestParam("productId")int productid){
-		
-		boolean overlap = cartSvc.CartOverlapCheck(productid);
-		Map<String,Boolean> map = new HashMap<>();
-		map.put("overlap",overlap);
-		return map;
-	}
-	
 	@PostMapping("addCart")
 	@ResponseBody
 	public Map<String,Boolean> addCart(@RequestParam("productId")int productid){
